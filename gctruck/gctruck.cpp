a43e75aeddde3f29e71d49fccce5cccfeba2976a
multiguy18 <anselm1793@hotmail.com>
2019-10-09 20:03:12 +0200

    Reorganized structures, added list pointers

--- gctruck/gctruck.cpp
+++ gctruck/gctruck.cpp
@@ -1,11 +1,14 @@
 #include <malloc.h>
 
-typedef struct observedptr_node_s {
-	void** observedptr;
-	struct observedptrnode_s* next;
-} observedptr_node_t;
+typedef struct trackedptr_node_s {
+	void** trackedptr;
+	struct trackedptr_node_s* next;
+} trackedptr_node_t;
 
 typedef struct allocated_node_s {
 	void* data;
 	struct allocated_node_s* next;
 } allocated_node_t;
+
+trackedptr_node_t* ptrlist;
+allocated_node_t* alloclist;
